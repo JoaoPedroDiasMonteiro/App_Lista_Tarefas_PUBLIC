55d7da403db003ca181ae3aa00f948b940385c92
João Pedro <joaopedro453@hotmail.com>
2019-11-08 21:05:06 -0300

    App Lista de Tarefas - Atualizando registros parte 1

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -13,10 +13,53 @@ require "./tarefa_controller.php";
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>App Lista Tarefas</title>
+	<style>
+		form {
+			margin-block-end: 0px ;
+			/* padding-left: 15px; */
+			padding: 0px 15px;
+		}
+	</style>
 
 	<link rel="stylesheet" href="css/estilo.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<script>
+	function editar(id, tarefa_txt) {		
+		// create elements
+		// form
+		let form = document.createElement('form')
+		form.action = '#'
+		form.method = 'post'
+		form.className = 'row'
+		// input
+		let input = document.createElement('input')
+		input.type ='text'
+		input.name = 'tarefa'
+		input.className = 'col-8 col-md-9 form-control'
+		input.value = tarefa_txt
+		// input hidden
+		let input_id = document.createElement('input')
+		input_id.type = 'hidden'
+		input_id.name = 'id'
+		input_id.value = id
+		// button
+		let button = document.createElement('button')
+		button.type = 'submit'
+		button.className = 'col-4 col-md-3 btn btn-info'
+		button.innerHTML = 'Atualizar'
+
+		// add elements to form
+		form.appendChild(input)
+		form.appendChild(button)
+		form.appendChild(input_id)
+
+		// add form to page
+		let tarefa = document.getElementById('tarefa_'+id)
+		tarefa.innerHTML = ''
+		tarefa.insertBefore(form, tarefa[0])
+	}
+	</script>
 </head>
 
 <body>
@@ -47,11 +90,11 @@ require "./tarefa_controller.php";
 							<hr />
 							<? foreach ($tarefas as $key => $value) { ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9"> <?= $value->tarefa ?> (<?= $value->status ?>)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
+								<div class="row mb-3 d-flex align-items-center tarefa py-5" style="background-color:#f2f2f2">
+									<div class="col-sm-9" id= '<?= 'tarefa_'.$value->id ?>'> <?= $value->tarefa ?> (<?= $value->status ?>)</div>
+									<div class="col-sm-3 d-flex mt-3 mt-sm-0 justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
+										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $value->id ?>, '<?= $value->tarefa ?>' )"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
