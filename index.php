b01b3de454cbf1c492a9367fc0ae55fc882d09a0
João Pedro <joaopedro453@hotmail.com>
2019-11-09 15:44:17 -0300

    hotfix

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ require "./tarefa_controller.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>App Lista Tarefas</title>
 
+	<style>
+		form {
+			margin-block-end: 0px;
+			/* padding-left: 15px; */
+			padding: 0px 15px;
+		}
+	</style>
 	<link rel="stylesheet" href="css/estilo.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
